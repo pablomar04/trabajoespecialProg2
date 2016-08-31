777b9834f4825d5c1f3208136a45d69bda1817c5
Juan Pablo Marcovecchio <pablo_mar04@hotmail.com>
2016-08-31 14:57:26 -0300

    Se creo la clase Pista

---
+++ src/tpe/Pista.java
@@ -0,0 +1,29 @@
+package tpe;
+
+public class Pista {
+	
+	private int ID;
+	private String titulo;
+	private int duracion;
+	private String artista;
+	private String album;
+	private int anio;
+	private String genero;
+	private String comentarios;
+	
+	public Pista(int iD, String titulo, int duracion, String artista, String album, int anio, String genero,
+			String comentarios) {
+		super();
+		ID = iD;
+		this.titulo = titulo;
+		this.duracion = duracion;
+		this.artista = artista;
+		this.album = album;
+		this.anio = anio;
+		this.genero = genero;
+		this.comentarios = comentarios;
+	}
+	
+	
+
+}
